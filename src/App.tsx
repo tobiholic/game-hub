5e3251c84b62a63b577b018f8454e8d801642d65
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-07-05 15:59:53 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <h1>hello World!</h1>
+        <p>this gonna be awesome </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
