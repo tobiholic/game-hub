5ac31801273ba41c569f81abeae612d9266fded8
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-07-13 16:46:45 +0800

    filter games by platform

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,13 +4,15 @@ import GameCardSkeleton from './GameCardSkeleton';
 import GameCard from './GameCard';
 import GameCardContainer from './GameCardContainer';
 import { Genre } from '../hooks/useGenres';
+import { Platform } from '../hooks/usePlatforms';
 
 interface Probs {
   selectedGenre: Genre | null;
+  selectedPlatform: Platform | null;
 }
 
-const GameGrid = ({ selectedGenre }: Probs) => {
-  const { data, error, isLoading } = useGames(selectedGenre);
+const GameGrid = ({ selectedGenre, selectedPlatform }: Probs) => {
+  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
   return (
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,7 +1,7 @@
 import useData from './useData';
 
-interface Platform {
-  id: string;
+export interface Platform {
+  id: number;
   name: string;
   slug: string;
 }
