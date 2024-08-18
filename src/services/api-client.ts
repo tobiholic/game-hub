8fcc136ff5f36df67f6a9a719b4de53c0066370d
tobiholic <123602167+tobiholic@users.noreply.github.com>
2024-08-18 23:01:54 +0800

    refactoring reusable API client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,4 +1,5 @@
-import axios from 'axios';
+import { fadeConfig } from '@chakra-ui/react';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export interface FetchResponse<T> {
   count: number;
@@ -6,9 +7,25 @@ export interface FetchResponse<T> {
   next?: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: 'fda5c5775a4c4a28b0388abc1efe3ca0',
   },
 });
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
